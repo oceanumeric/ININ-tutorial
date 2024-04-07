3581f4ae4925bd8673a01f69b4a9a268c157646b
Michael WANG Fei <79036899+oceanumeric@users.noreply.github.com>
2024-04-07 21:05:53 +0000

    [add] install bash

---
+++ install.sh
@@ -0,0 +1,12 @@
+# update
+apt-get update && sudo apt-get upgrade
+
+# install gfortran
+apt-get install gfortran
+
+
+# r install 'vtree'
+Rscript -e 'install.packages("vtree")'
+
+# r install 'psych'
+Rscript -e 'install.packages("psych")'
